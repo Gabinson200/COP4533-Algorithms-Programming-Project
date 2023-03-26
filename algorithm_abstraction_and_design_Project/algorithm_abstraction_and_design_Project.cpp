f5b6db6bdde0ca3d058fa49c88fa0817fbaa50e1
Adam <a.kocsis@ufl.edu>
2023-03-26 03:36:37 -0400

    added problem 1 algo 3b

--- algorithm_abstraction_and_design_Project/algorithm_abstraction_and_design_Project.cpp
+++ algorithm_abstraction_and_design_Project/algorithm_abstraction_and_design_Project.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+//#include <bits/stdc++.h>
 #include <vector>
+#include <algorithm>
 #include <random>
 
 using namespace std;
@@ -22,11 +24,11 @@ void Alg1(vector<vector<int>>& A, int* stock, int* buy_day, int* sell_day) {
         for (int j = 1; j < n; j++) {
             if (A[i][j] >= A[i][sell]) {
                 sell = j;
-            }
-            //in the column find the minimum value before the position of the maximum value
-            for (int k = 0; k < sell; k++) {
-                if (A[i][k] < A[i][buy]) {
-                    buy = k;
+                //in the column find the minimum value before the position of the maximum value
+                for (int k = 0; k < sell; k++) {
+                    if (A[i][k] < A[i][buy]) {
+                        buy = k;
+                    }
                 }
             }
         }
@@ -80,6 +82,73 @@ void Alg2(vector<vector<int>>& A, int* stock, int* buy_day, int* sell_day) {
         }
     }
 }
+
+/*
+//Dynamic programming using recursion and memoization O(m*n)
+int Alg3a(vector<vector<int>>& A, int* stock, int* buy_day, int* sell_day, int day) {
+    //set m number of stocks with n number of days
+    int m = A.size();
+    int n = A[0].size();
+    cout << "Vector size: " << m << "x" << n << endl;
+    int profit = 0;
+
+    if (day == n) return 0; 
+    if (memo[day] != -1) return memo[day];
+
+    int ans = 0, min_price = A[0][day];
+    for (int i = 1; i < m; i++) {
+        min_price = min(min_price, A[i][day]);
+        ans = max(ans, A[i][day] - min_price);
+    }
+
+    return memo[day] = max(ans, Alg3a(day + 1));
+  
+}
+*/
+
+void Alg3b(vector<vector<int>>& A, int* stock, int* buy_day, int* sell_day) {
+    int m = A.size();
+    int n = A[0].size();
+    cout << "Vector size: " << m << "x" << n << endl;
+    //allocate the arrays
+    int** dp = new int* [m];
+    for (int i = 0; i < m; i++)
+        dp[i] = new int[n];
+
+    int** bd = new int* [m];
+    for (int i = 0; i < m; i++)
+        bd[i] = new int[n];
+
+    int profit = 0;
+    int buy_val = 1e3+5;
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 1; j < n; j++) { 
+            dp[i][j] = dp[i][j - 1] + A[i][j] - A[i][j - 1];
+
+            if (dp[i][j] > profit) {
+                profit = dp[i][j];
+                *stock = i;
+                *sell_day = j;
+                *buy_day = bd[i][j - 1];
+            }
+            bd[i][j] = bd[i][j - 1];
+            if (dp[i][j - 1] < 0) {
+                bd[i][j] = j - 1;
+            }
+         
+        }
+    }
+    //deallocate the arrays
+    for (int i = 0; i < m; i++)
+        delete[] dp[i];
+    delete[] dp;
+
+    for (int i = 0; i < m; i++)
+        delete[] bd[i];
+    delete[] bd;
+}
+
 
 vector<vector<int>> problem_one_file_to_vec(string filename) {
     ifstream infile(filename);
@@ -152,8 +221,8 @@ int main() {
     {
         {7, 6, 22, 1},
         {1, 0, 3, 8},
-        {4, 5, 100, 10},
-        {25, 8, 9, 1 }
+        {8, 5, 100, 10},
+        {25, 8, 1, 100}
     };
 
     cout << "algos on A" << endl;
@@ -161,15 +230,21 @@ int main() {
     cout << stock << " " << buy_day << " " << sell_day << endl;
     Alg2(A, &stock, &buy_day, &sell_day);
     cout << stock << " " << buy_day << " " << sell_day << endl;
+    Alg3b(A, &stock, &buy_day, &sell_day);
+    cout << stock << " " << buy_day << " " << sell_day << endl;
     cout << "algos on B" << endl;
     Alg1(B, &stock, &buy_day, &sell_day);
     cout << stock << " " << buy_day << " " << sell_day << endl;
     Alg2(B, &stock, &buy_day, &sell_day);
     cout << stock << " " << buy_day << " " << sell_day << endl;
+    Alg3b(B, &stock, &buy_day, &sell_day);
+    cout << stock << " " << buy_day << " " << sell_day << endl;
     cout << "algos on C" << endl;
     Alg1(C, &stock, &buy_day, &sell_day);
     cout << stock << " " << buy_day << " " << sell_day << endl;
     Alg2(C, &stock, &buy_day, &sell_day);
+    cout << stock << " " << buy_day << " " << sell_day << endl;
+    Alg3b(C, &stock, &buy_day, &sell_day);
     cout << stock << " " << buy_day << " " << sell_day << endl;
 
 
@@ -182,6 +257,8 @@ int main() {
     cout << stock << " " << buy_day << " " << sell_day << endl;
     Alg2(P1_5_10, &stock, &buy_day, &sell_day);
     cout << stock << " " << buy_day << " " << sell_day << endl;
+    Alg3b(P1_5_10, &stock, &buy_day, &sell_day);
+    cout << stock << " " << buy_day << " " << sell_day << endl;
 
     cout << endl << "NOW TESTING P1 WITH READ FILE FUNCTION AND RANDOMLY GENERATED VALUES 10 by 20" << endl;
 
@@ -191,6 +268,8 @@ int main() {
     Alg1(P1_10_20, &stock, &buy_day, &sell_day);
     cout << stock << " " << buy_day << " " << sell_day << endl;
     Alg2(P1_10_20, &stock, &buy_day, &sell_day);
+    cout << stock << " " << buy_day << " " << sell_day << endl;
+    Alg3b(P1_10_20, &stock, &buy_day, &sell_day);
     cout << stock << " " << buy_day << " " << sell_day << endl;
 
     return 0;
